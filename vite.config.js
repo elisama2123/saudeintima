e87f6f705c6998aa10b153a31e374595fac8b0c0
elisamafisiopelvica <elisamafisiopelvica@gmail.com>
2025-06-15 19:01:00 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/saudeintima/", // 👈 ESSA LINHA AQUI É ESSENCIAL!
   server: {
     host: "::",
     port: "8080",
